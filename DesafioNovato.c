bed099a9f501b71b0b5cec6743072b85e798050f
_lsants_ <Lauro.sants@Gmail.com>
2025-01-08 18:52:03 +0000

    Desafioaventureiro

--- DesafioNovato.c
+++ DesafioNovato.c
@@ -10,8 +10,10 @@
             char cidade[20];
             int populacao;
             int pontos_turisticos;
-            double area;
-            double pib;
+            float area;
+            float pib;
+            float PIBCapita;
+            float densidade;
 
         printf("Olá, Seja bem vindo ao jogo Super Trunfo: Países!\nVamos começar cadastrando cartas, é a base do jogo.\n");
 
@@ -31,21 +33,28 @@
                 scanf("%d", &populacao);
 
                 printf("Área (em km^2):\n");
-                scanf("%lf", &area);
+                scanf("%f", &area);
 
-                printf("PIB em bilhões (use .):\n");
-                scanf("%lf", &pib);
+                printf("PIB:\n");
+                scanf("%f", &pib);
 
                 printf("Número de Pontos Turísticos:\n");
                 scanf("%d", &pontos_turisticos);
+
+                    //calculando medias
+                densidade = (float)populacao / area;
+                PIBCapita = (float)pib / populacao;
+                
 
                     // Exibindo os dados da carta cadastrada
                 printf("\n=== Carta Cadastrada ===\n");
                 printf("Carta: %s - %s\n", cidade, estado);
                 printf("População: %d\n", populacao);
                 printf("Área: %.2f km^2\n", area);
-                printf("PIB: R$ %.2f bilhões\n", pib);
+                printf("PIB: R$ %.2f\n", pib);
                 printf("Número de Pontos Turísticos: %d\n", pontos_turisticos);
+                printf("Densidade Populacional: %f\n", densidade);
+                printf("PIB per Capita: %f\n", PIBCapita);
                 printf("---------------------------\n");
 
 
